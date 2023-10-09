07bf885279530f80556ef3f68908c5a6bbe1e445
wwrena <teikukuheia@gmail.com>
2023-10-09 23:37:43 +0300

    Added prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 150
+};
+
+module.exports = config;
