6b32b91c534c48fb9c238a4585d8dc6032600b27
wwrena <teikukuheia@gmail.com>
2023-10-31 18:33:29 +0200

    Added firebasestorage to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
+  },
   experimental: {
     serverActions: true,
   },
