e857144c2120573399d5ba8a1859bb162aa98cef
wwrena <teikukuheia@gmail.com>
2023-12-22 02:05:43 +0200

    Added websocket config

--- common/socket.ts
+++ common/socket.ts
@@ -3,4 +3,11 @@ import { io } from 'socket.io-client';
 
 const URL = `${HOST_DNS}:3002`;
 
-export const socket = io(URL);
+export const socket = io(URL, {
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 10000,
+  autoConnect: true,
+  transports: ['websocket'],
+  rejectUnauthorized: true,
+});
